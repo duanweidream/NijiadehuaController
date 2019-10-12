c6b3d9fd951b9ad92b781aca108a80314faa0e64
duanwei <duanwei@wobmob.cn>
2019-10-12 12:18:01 +0800

    清理项目多余文件

--- WebRoot/pages/static/js/common/bee-footer.js
+++
@@ -1,8 +0,0 @@
-document.writeln('<div class="page-footer">');
-document.writeln('<div class="page-footer-inner"> 2017 &copy; HiveData.Express by wooboo.');
-document.writeln('<a href="#" title="" target="_blank">About HiveData!</a>');
-document.writeln('</div>');
-document.writeln('<div class="scroll-to-top">');
-document.writeln(' <i class="icon-arrow-up"></i>');
-document.writeln('</div>');
-document.writeln('</div>');
